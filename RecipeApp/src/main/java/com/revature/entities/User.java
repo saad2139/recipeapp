50e4199f67d1686e6eb17ffbc72cb09320f7d99e
Kelton Keener <keltonkeener@gmail.com>
2017-12-05 09:44:41 -0500

    New Project

--- RecipeApp/src/main/java/com/revature/entities/User.java
+++ RecipeApp/src/main/java/com/revature/entities/User.java
@@ -1,9 +1,5 @@
 package com.revature.entities;
 
 public class User {
-<<<<<<< HEAD
-	private String firstname;
-=======
-	private String FirstName;
->>>>>>> 56d3b5e76ea13d6dee72c4d555dcd6b41813c14c
+
 }
