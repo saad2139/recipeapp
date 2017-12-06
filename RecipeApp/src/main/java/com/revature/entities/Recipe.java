cb6269edd5c477819074258d2aaaffb60224478e
saad2139 <saad2139@gmail.com>
2017-12-05 20:12:45 -0500

    Update Recipe.java

--- RecipeApp/src/main/java/com/revature/entities/Recipe.java
+++ RecipeApp/src/main/java/com/revature/entities/Recipe.java
@@ -2,16 +2,7 @@ package com.revature.entities;
 
 import java.sql.Timestamp;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-import javax.persistence.SequenceGenerator;
+import javax.persistence.*;
 
 @Entity
 public class Recipe {
