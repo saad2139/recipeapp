13bafc35f50c413c88dca9e3dcf76b21f51e59a2
DESKTOP-AD7EOVG\Saad <saad2139@gmail.com>
2017-12-06 19:27:10 -0500

    Changed pom.xml

--- RecipeApp/src/main/java/com/revature/entities/Review.java
+++ RecipeApp/src/main/java/com/revature/entities/Review.java
@@ -24,6 +24,37 @@ public class Review {
 		this.rating = rating;
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + rating;
+		result = prime * result + reviewId;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Review other = (Review) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (rating != other.rating)
+			return false;
+		if (reviewId != other.reviewId)
+			return false;
+		return true;
+	}
+
 	public int getReviewId() {
 		return reviewId;
 	}
