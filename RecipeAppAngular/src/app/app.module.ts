ccc140450e102605608f21aad81a7ce12a92a6a4
DESKTOP-AD7EOVG\Saad <saad2139@gmail.com>
2017-12-08 14:25:24 -0500

    Added Angular Template

---
+++ RecipeAppAngular/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { UIRouterModule } from '@uirouter/angular';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import {AppComponent} from './app.component';
+import {NavComponent} from './nav/nav.component';
+
+import { appRoutes } from './routes';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    HttpModule,
+    NgbModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
+    FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    NavComponent,
+   ],
+  providers: [
+
+   ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
